561f358b0107e4766f43e88d0807757a09c46c11
Omar Valenzuela <omar_valenzuela@migesa.com.mx>
2019-03-13 09:43:52 -0600

    modificaciones para deploy Heroku

--- ReclutamientoBE/models/db.js
+++ ReclutamientoBE/models/db.js
@@ -4,13 +4,15 @@ const pgp = require('pg-promise')({
     promiseLib: promise
 });
 
-const conn = {
+/* const conn = {
     host: 'localhost',
     port: '5432',
     database: 'Reclutamiento',
     user: 'postgres',
     password: 'migesa_admin'
-};
+}; */
+const dev_conn_str = 'postgress://postgres:migesa_admin@localhost:5432/Reclutamiento';
+const conn = process.env.POSTGRES_URI || dev_conn_str;
 
 const db = pgp(conn);
 
